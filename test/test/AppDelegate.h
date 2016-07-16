8bf436fa5721bf35b449ca94e5de34c93f78a854
Jason <jason@cobiinteractive.com>
2016-07-16 11:12:12 +0200

    Initial commit

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Bob Ridge on 2016/07/16.
+//  Copyright © 2016 Yoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Bob Ridge on 2016/07/16.
+//  Copyright © 2016 Yoco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
